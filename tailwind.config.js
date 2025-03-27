9ee2688cfaa398e99e1a0dd490d96e4f60d2aeb7
azuazu1313 <asnismarts7@gmail.com>
2025-03-27 03:52:26 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        optima: ['Optima', 'sans-serif'],
+        'optima-italic': ['Optima Italic', 'serif'],
+        'optima-bold': ['Optima Bold', 'sans-serif'],
+        unna: ['Unna', 'serif'],
+        'unna-italic': ['Unna Italic', 'serif'],
+        'unna-bold': ['Unna Bold', 'serif'],
+        'unna-bold-italic': ['Unna Bold Italic', 'serif'],
+        birthstone: ['Birthstone', 'serif'],
+        nunitosans: ['NunitoSans', 'sans-serif'],
+        'nunitosans-italic': ['NunitoSans Italic', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
